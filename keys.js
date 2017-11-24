28f0c5edc1a82279879615b932e1a14580148cfc
Collin Cavallo <collincavallo@gmail.com>
2017-11-23 19:16:11 -0600

    pushing from last weeks additions

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'Ne9uB8ueFSp1qBKwZ3Tz65qw3',
+  consumer_secret: '3IGyOQrVBOAV19h6XvipPGhH9ILNl47ESvMzN2tt11L2iN8Icj',
+  access_token_key: '932654708333993984-xVDQLSYQMvzWrHWyTjweRmANGw5oI2i',
+  access_token_secret: '9xiDpkkCK94wu4qsvGtgm59LMPNOB7nwZvlBjnUuJGzbl',
+};
+var spotifyKeys = {
+    id: '945126bd61b54bfb8eee4cd2e737acbf',
+    secret: 'd43aec91e32346078de9efb8f9191b44'
+}
+
+module.exports = {
+    twitterKeys:twitterKeys,
+    spotifyKeys:spotifyKeys
+};
---
+++ liri.js
@@ -0,0 +1,77 @@
+var keys = require("./keys.js");
+var request = require("request")
+var fs = require("fs")
+var Twitter = require("twitter")
+var Spotify = require("node-spotify-api")
+var input = process.argv;
+var input1 = input[2];
+var input2 = input[3];
+
+
+
+function myTweets() {
+    var client = new Twitter(keys.twitterKeys);
+    console.log('tweets');
+    client.get("statuses/user_timeline", function (error, tweets, response) {
+        if (error) {
+            console.log(error);
+        }
+
+        tweets.forEach(function (tweets) {
+            console.log(tweets.text + " " + tweets.created_at);
+        })
+    })
+}
+function spotifySong (song) {
+    if (song === " "){
+        song === "The Sign Ace of Base"
+    }
+    var spotifySearch = new Spotify(keys.spotifyKeys);
+    console.log('spotify');
+    spotifySearch.search({
+        type: 'track',
+        query: song,
+        limit: 1
+    },
+    function(err, data) {
+    if (err) {
+        console.log(error)
+    }
+    console.log("worked")
+    console.log(data.tracks.items[0].album.artists[0].name);
+    console.log(data.tracks.items[0].album.name);
+    console.log(data.tracks.items[0].name);
+    console.log(data.tracks.items[0].preview_url);
+
+    })
+}
+function movieInfo(){
+
+}
+function readRandom(){
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (error){
+            console.log(error)
+        }
+        console.log(data);
+    })
+}
+
+function cmdListen(request, userInput) {
+    switch (request) {
+        case "my-tweets":
+            myTweets();
+            break;
+        case "spotify-this-song":
+            spotifySong(userInput);
+            break;
+        case "movie-this":
+            movieInfo(userInput);
+            break;
+        case "do-what-it-says":
+            readRandom();
+            break;
+
+    }
+}
+cmdListen(input1, input2);
